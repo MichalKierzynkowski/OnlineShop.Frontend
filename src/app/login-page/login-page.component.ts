fd11d9107a292017f0f41e26aad33572fd2964fc
ktomkow <krzysztof.tomkow@gmail.com>
2022-04-04 20:58:08 +0200

    Use TS interface instead of type Any

--- src/app/login-page/login-page.component.ts
+++ src/app/login-page/login-page.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-
+import { LoginCredentials } from '../models/credentials';
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
   styleUrls: ['./login-page.component.scss'],
 })
 export class LoginPageComponent implements OnInit {
-  credentials: any = {
+  credentials: LoginCredentials = {
     login: '',
     password: '',
   };
